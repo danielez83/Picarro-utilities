f0c4e17218610a5554bafacd438bb3fa0bfd1271
danielez83 <daniele.zannoni>
2024-03-21 16:26:26 +0100

    Change to col num to col name. Automatical remove of non .dat files

--- import_picarro_raw_data.py
+++ import_picarro_raw_data.py
@@ -33,27 +33,33 @@ def import_picarro_raw_data(Picarro_data_path, op_mode):
     import numpy as np
     import os
     # Mode select
+    col_names = ['DATE', 'TIME', 'H2O', 'Delta_18_16', 'Delta_D_H', 'ValveMask']
+    col_names_extra = ['CavityPressure', 'CavityTemp', 'WarmBoxTemp', 'DasTemp', 'EtalonTemp', 'MPVPosition', 'OutletValve']
+    #col_names = col_names + col_names_extra
     if op_mode  == 'normal':
         # Variable position in raw data file
-        col_nums = (0,1,2,8,15,16,17,21)
+        #col_nums = (0,1,2,8,15,16,17,21)
+        pass
     elif op_mode == '17O':
         # Variable position in raw data file
-        col_nums = (0,1,2,8,9,10,11,12,14,15,16,17,18)
+        #col_nums = (0,1,2,8,9,10,11,12,14,15,16,17,18)
+        col_names = col_names + ['Delta_17_16']
     # List files in directory
     data_filenames = os.listdir(Picarro_data_path)
     Picarro_data = pd.DataFrame()     
     # Import files
     for file in data_filenames:
-        print('Reading: ', Picarro_data_path + file)
-        df_dummy = pd.read_csv(Picarro_data_path + file,
-                               delim_whitespace = True,
-                               index_col = (0),
-                               parse_dates=[[0, 1]],
-                               infer_datetime_format=True ,
-                               na_values=['NAN'],
-                               usecols=col_nums,
-                               dtype={'FRAC_DAYS_SINCE_JAN1': np.float64})
-        Picarro_data = pd.concat([Picarro_data, df_dummy], axis = 0)
+        if file[-4:] == '.dat': # skip non-dat files
+            print('Reading: ', Picarro_data_path + file)
+            df_dummy = pd.read_csv(Picarro_data_path + file,
+                                   delim_whitespace = True,
+                                   index_col = (0),
+                                   parse_dates=[[0, 1]],
+                                   infer_datetime_format=True ,
+                                   na_values=['NAN'],
+                                   usecols=col_names,
+                                   dtype={'FRAC_DAYS_SINCE_JAN1': np.float64})
+            Picarro_data = pd.concat([Picarro_data, df_dummy], axis = 0)
     # Sort observation by index
     Picarro_data.index.name='Date'
     Picarro_data.sort_values(axis = 0, by = 'Date', inplace=True) # This now sorts in date order 
